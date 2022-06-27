26ca65b33fe809bb17cb756c3c3999f0eb3e7aef
joon bae <wnsqoqo@gmail.com>
2022-06-27 00:35:29 -0500

    modal fixed

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,19 +1,39 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Modal } from '../../context/Modal';
 import LoginForm from './LoginForm';
+import * as sessionActions from '../../store/session'
+import './LoginForm.css'
 
 function LoginFormModal() {
+  const dispatch = useDispatch();
+
   const [showModal, setShowModal] = useState(false);
+  const [credential, setCredential] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // setErrors([]);
+    // setCredential('FakeUser1')
+    // setPassword('password2')
+    return dispatch(sessionActions.login({ credential:'FakeUser1', password:'password2' }))
+  };
 
   return (
     <>
-      <button onClick={() => setShowModal(true)}>Log In</button>
-      {showModal && (
-        <Modal onClose={() => setShowModal(false)}>
-          <LoginForm />
-        </Modal>
-      )}
-    </>
+    <button  className='main-login-button' onClick={() => setShowModal(true)}>
+      <img className='hamburger-menu' src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png'></img>
+      </button>
+    {showModal && (
+      <Modal onClose={() => setShowModal(false)}>
+        <LoginForm />
+        <div id='demo-login-modal'>
+        <button type='submit' className='demo-login-button' onClick={(e) => handleSubmit(e)}>Log In As Demo User</button>
+        </div>
+      </Modal>
+    )}
+  </>
   );
 }
 
