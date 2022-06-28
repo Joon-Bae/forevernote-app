b61b24f5c47afd6de6b31c697ff911b4dd3eddf3
joon bae <wnsqoqo@gmail.com>
2022-06-28 08:57:28 -0500

    homepage component created

---
+++ frontend/src/components/Homepage/index.js
@@ -0,0 +1,6 @@
+import "./home.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getBusinesses } from "../../store/business";
+import { useEffect } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+
