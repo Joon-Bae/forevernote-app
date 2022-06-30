aa91fd88c2514f698122ad3fe2b638a07b7165a4
joon bae <wnsqoqo@gmail.com>
2022-06-30 00:27:19 -0500

    bugs debugged

---
+++ backend/db/seeders/20220630051622-Notebooks.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('NoteBooks', [
+    { userId:2, title:"hi", createdAt:new Date(), updatedAt:new Date()}
+   ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('NoteBooks', null, {});
+  }
+};
